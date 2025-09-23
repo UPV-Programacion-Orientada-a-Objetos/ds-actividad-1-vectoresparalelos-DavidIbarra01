4d3f2732531795fd693ee3660d663a96ee8d8a98
DavidIbarra01 <2430249@upv.edu.mx>
2025-09-22 21:53:43 -0600

    Se reemplazo el while por el switch hasta registro

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 //El Prototipo
 #include <iostream>
-#include <vector>
-#include <string>
+#include <vector> //Para utilizar metodos de vectores, como el push_back
+#include <string> 
+#include <sstream> //Para manipular las cadenas de caracteres
+#include <fstream> //Proporciona las herramientas necesarias para la lectura de archivos CSV
+#define nombre_archivo "list.csv"
 //PROBLEMA PROBLEMON: Registro de Atletas Olímpicos
 
 
@@ -23,78 +26,71 @@
 
 
 int main() {
+    
  //Mis variables
+std::string nombres_atletas[50];
+std::string paises_origen[50];
+std::string disciplinas[50];
+char generos[50];
+int cantidades_medallas[50];
+  int select;
+int numero_atletas = 0;
 
-    std::vector<std::string> nombres_atletas;
-     std::vector<std::string> pais_origen;
-     std::vector<std::string> disciplina;
-    std::vector<char> genero;
-    std::vector<int> cantidad_medallas;
-
- 
-
-while (true)
-    {
-    std::cout << "1) Registrar Atleta." << std::endl;
+do
+{
+  std::cout << "1) Registrar Atleta." << std::endl;
     std::cout << "2) Buscar Atleta." << std::endl;
     std::cout << "3) Calcular el total de medallas de oro del pais." << std::endl;
     std::cout << "4) Encontrar Atleta con la mayor cantidad de medallas." << std::endl;
      std::cout <<"5) Salir" << std::endl;
-    int select = 0;
-//Validar la seleccion del usuario
+ std::cin >> select;
+
+ switch (select)
+ {
+ case 1:
+    std::cout << "Ingrese el nombre del atleta:\n";
+    std::cin.ignore(); //Limpia el cin
+    std::getline(std::cin, nombres_atletas[numero_atletas]);
     
-         std::cin >> select;
-    if (select < 1 or select > 5)
+    std::cout << "Su pais de origen:\n";
+    std::getline(std::cin, paises_origen[numero_atletas]);
+
+    std::cout << "Su disciplina:\n";
+    std::getline(std::cin, disciplinas[numero_atletas]);
+
+    std::cout << "Su genero: (M/F)\n";
+   std::cin >> generos[numero_atletas];
+
+    std::cout << "Y por ultimo, la cantidad de medallas que posee:\n";
+
+    //Validacion de cantidad positiva en las medallas
+    while (true)
     {
-       std::cout << "Dato invalido\n";
-    } else if (select == 1) //REGISTRAR
-    {
+        std::cin >> cantidades_medallas[numero_atletas];
+        if (cantidades_medallas[numero_atletas] < 0)
+        {
+            std::cout << "¡ERROR! Ingrese un numero positivo\n";
+        }else{
+            break;
+        }
         
-        std::cout << "Ingrese el nombre del atleta:\n";
-        std::string name = "";
-        std::cin.ignore();
-        std::getline(std::cin, name);
-        nombres_atletas.push_back(name);
-
-        std::cout << "Su pais de origen:\n";
-        std::string pai;
-        std::getline(std::cin, pai);
-        pais_origen.push_back(pai);
-
-        std::cout << "Su disciplina:\n";
-       std::string dis;
-        std::getline(std::cin, dis);
-        disciplina.push_back(dis);
-
-        std::cout << "Su genero:\n";
-        char gen;
-       std::cin >> gen;
-       genero.push_back(gen);
-
-        std::cout << "Y por ultimo la cantidad de medallas:\n";
-        int meda;
-       std::cin >> meda;
-       cantidad_medallas.push_back(meda);
-
-
-    }else if (select == 2)
-    {
-      
-     
-    }else if (select == 3)
-    {
-        
-      
-    }else if (select == 4)
-    {
-      
-     
-    }else if (select == 5)
-    {
-         std::cout << "Gracias por utilizar este programa :3." << std::endl;
-      break;
     }
     
-    } //WHILE
+
+    numero_atletas++;
+    break; //registro
+ 
+
+    case 2: //La busqueda de atletas :)
+
+    break;
+ default:
+ std::cout << "Ingrese una opcion valida\n";
+    break;
+ }
+
+} while (true); //EL CORAZON DEL PROGRAMA
+
+    std::cout << "Gracias por haber utilizado el programa :3\n";
     return 0;
 }
