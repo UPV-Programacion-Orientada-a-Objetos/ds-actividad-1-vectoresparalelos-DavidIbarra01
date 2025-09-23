7ed9369872e9ba2ff4a4a63e3de0cae9c98b0fe2
DavidIbarra01 <2430249@upv.edu.mx>
2025-09-22 20:38:22 -0600

    Registro hecho

--- main.cpp
+++ main.cpp
@@ -24,43 +24,77 @@
 
 int main() {
  //Mis variables
-    std::vector<std::string> nombres_atletas = {};
+
+    std::vector<std::string> nombres_atletas;
      std::vector<std::string> pais_origen;
      std::vector<std::string> disciplina;
     std::vector<char> genero;
     std::vector<int> cantidad_medallas;
 
-std::cout << nombres_atletas[0];
+ 
 
+while (true)
+    {
     std::cout << "1) Registrar Atleta." << std::endl;
     std::cout << "2) Buscar Atleta." << std::endl;
     std::cout << "3) Calcular el total de medallas de oro del pais." << std::endl;
     std::cout << "4) Encontrar Atleta con la mayor cantidad de medallas." << std::endl;
+     std::cout <<"5) Salir" << std::endl;
     int select = 0;
 //Validar la seleccion del usuario
-    while (true)
-    {
+    
          std::cin >> select;
-    if (select < 1 or select > 4)
+    if (select < 1 or select > 5)
     {
        std::cout << "Dato invalido\n";
-    } else if (select == 1)
+    } else if (select == 1) //REGISTRAR
     {
         
+        std::cout << "Ingrese el nombre del atleta:\n";
+        std::string name = "";
+        std::cin.ignore();
+        std::getline(std::cin, name);
+        nombres_atletas.push_back(name);
+
+        std::cout << "Su pais de origen:\n";
+        std::string pai;
+        std::getline(std::cin, pai);
+        pais_origen.push_back(pai);
+
+        std::cout << "Su disciplina:\n";
+       std::string dis;
+        std::getline(std::cin, dis);
+        disciplina.push_back(dis);
+
+        std::cout << "Su genero:\n";
+        char gen;
+       std::cin >> gen;
+       genero.push_back(gen);
+
+        std::cout << "Y por ultimo la cantidad de medallas:\n";
+        int meda;
+       std::cin >> meda;
+       cantidad_medallas.push_back(meda);
+
+
     }else if (select == 2)
     {
       
-       break;
+     
     }else if (select == 3)
     {
         
-       break;
+      
     }else if (select == 4)
     {
       
-       break;
+     
+    }else if (select == 5)
+    {
+         std::cout << "Gracias por utilizar este programa :3." << std::endl;
+      break;
     }
     
-    }
+    } //WHILE
     return 0;
 }
