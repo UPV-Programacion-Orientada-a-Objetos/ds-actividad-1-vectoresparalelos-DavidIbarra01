b2502d999498ed438c3efa061a828393dac8ae87
DavidIbarra01 <2430249@upv.edu.mx>
2025-09-22 20:02:36 -0600

    Remodelacion

--- main.cpp
+++ main.cpp
@@ -4,40 +4,7 @@
 #include <string>
 //PROBLEMA PROBLEMON: Registro de Atletas Olímpicos
 
-void menu();
 
-
-void Registrar(std::vector<std::string>&nombres,  std::vector<std::string>&paises,  std::vector<std::string>&disciplina,  std::vector<char>&genero,  std::vector<int>&medallas){
-    std::string name;
-std::cout << "Ingrese el nombre del atleta:\n";
-std::cin.ignore();
-std::getline(std::cin, name);
-nombres.push_back(name);
-
-std::cout << "El pais de origen:\n";
-std::string pais;
-std::getline(std::cin, pais);
-paises.push_back(pais);
-menu();
-}
-
-void Buscar(){
-
-}
-
-void CalcularTotalMedallas(){
-
-}
-//Buscar el atleta con mayor cantidad de medallas
-void AtletaHardcore(){
-
-}
-
-
-
-
-void menu(){
-   
 
     /*std::cout << "+----------------------------------------------------+" << std::endl;
 |                       FEDERACIÓN MUNDIAL DE ATLETISMO                        |
@@ -53,12 +20,17 @@ void menu(){
 +-------------------------------------------------------------------------------+
 ";*/
 
+
+
+int main() {
  //Mis variables
-    std::vector<std::string> nombres_atletas;
+    std::vector<std::string> nombres_atletas = {};
      std::vector<std::string> pais_origen;
      std::vector<std::string> disciplina;
     std::vector<char> genero;
     std::vector<int> cantidad_medallas;
+
+std::cout << nombres_atletas[0];
 
     std::cout << "1) Registrar Atleta." << std::endl;
     std::cout << "2) Buscar Atleta." << std::endl;
@@ -74,28 +46,21 @@ void menu(){
        std::cout << "Dato invalido\n";
     } else if (select == 1)
     {
-        Registrar(nombres_atletas, pais_origen, disciplina, genero, cantidad_medallas);
-       break;
-    }else if (select == 1)
+        
+    }else if (select == 2)
     {
-        Buscar();
+      
        break;
-    }else if (select == 1)
+    }else if (select == 3)
     {
-        CalcularTotalMedallas();
+        
        break;
-    }else if (select == 1)
+    }else if (select == 4)
     {
-        AtletaHardcore();
+      
        break;
     }
     
     }
-    
-}
-
-
-int main() {
-menu();
     return 0;
 }
